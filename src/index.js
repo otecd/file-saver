9bd3b40bea59e7d733653844a355351936e0d029
Dmitriy <idimzz@gmail.com>
2019-10-22 20:08:55 +0000

    use a constructor instead of methods

--- src/index.js
+++ src/index.js
@@ -25,70 +25,82 @@ const download = (url, dest) => new Promise((resolve, reject) => {
   )
 })
 
-export const saveRemote = (imgUrl) => {
-  const newFileName = `${uuidv4(imgUrl)}.jpg`
+export default class ImageSaver {
+  constructor ({
+    url,
+    request,
+    onlyReplacement,
+    blur
+  } = {}) {
+    if (url) {
+      const newFileName = `${uuidv4(url)}.jpg`
 
-  return download(imgUrl, path.join(__dirname, '../img', newFileName))
-    .then(() => newFileName)
-}
+      if (blur) {
+        const tempFilePath = path.join(__dirname, '../img', `temp_${newFileName}`)
 
-export const saveBlurredRemote = (imgUrl) => {
-  const newFileName = `${uuidv4(imgUrl)}.jpg`
-  const tempFilePath = path.join(__dirname, '../img', `temp_${newFileName}`)
+        return download(url, tempFilePath)
+          .then(() => sharp(tempFilePath)
+            .resize(100, 100)
+            .blur(3)
+            .jpeg({ quality: 60 })
+            .toFile(path.join(__dirname, '../img', newFileName)))
+          .then(() => {
+            fs.unlink(tempFilePath, () => {})
 
-  return download(imgUrl, tempFilePath)
-    .then(() => sharp(tempFilePath)
-      .resize(100, 100)
-      .blur(3)
-      .jpeg({ quality: 60 })
-      .toFile(path.join(__dirname, '../img', newFileName)))
-    .then(() => {
-      fs.unlink(tempFilePath, () => {})
+            return newFileName
+          })
+      }
 
-      return newFileName
-    })
-}
-
-export const saveInput = (request, imagesAmount) => new Promise((resolve, reject) => {
-  const form = new IncomingForm({ uploadDir: path.join(__dirname, '../img'), keepExtensions: true })
-
-  form.on('file', (_, file) => {
-    if (file.name === 'temp.jpg' && imagesAmount === 6) {
-      return reject(new RichError('Images limit is reached', 416, codes.REACHED_IMAGES_LIMIT))
+      return download(url, path.join(__dirname, '../img', newFileName))
+        .then(() => newFileName)
     }
 
-    const oldFileName = file.name !== 'temp.jpg' && file.name
-    const newFileName = `${uuidv4()}.jpg`
+    if (request) {
+      return new Promise((resolve, reject) => {
+        const form = new IncomingForm({ uploadDir: path.join(__dirname, '../img'), keepExtensions: true })
 
-    jo.rotate(file.path, { quality: 90 })
-      .then(({ buffer }) => {
-        fs.writeFile(path.join(__dirname, '../img', newFileName), buffer, (error) => {
-          if (error) {
-            return reject(error)
+        form.on('file', (_, file) => {
+          if (file.name === 'temp.jpg' && onlyReplacement) {
+            return reject(new RichError('Images limit is reached', 416, codes.REACHED_IMAGES_LIMIT))
           }
-          return oldFileName
-            ? fs.unlink(path.join(__dirname, '../img', oldFileName), () => resolve({ newFileName, oldFileName }))
-            : resolve({ newFileName })
+
+          const oldFileName = file.name !== 'temp.jpg' && file.name
+          const newFileName = `${uuidv4()}.jpg`
+
+          jo.rotate(file.path, { quality: 90 })
+            .then(({ buffer }) => {
+              fs.writeFile(path.join(__dirname, '../img', newFileName), buffer, (error) => {
+                if (error) {
+                  return reject(error)
+                }
+                return oldFileName
+                  ? fs.unlink(path.join(__dirname, '../img', oldFileName), () => resolve({ newFileName, oldFileName }))
+                  : resolve({ newFileName })
+              })
+            })
+            .catch(() => {
+              fs.rename(
+                file.path,
+                path.join(__dirname, '../img', newFileName),
+                (error) => {
+                  if (error) {
+                    reject(error)
+                  } else {
+                    if (oldFileName) {
+                      fs.unlink(path.join(__dirname, '../img', oldFileName), () => resolve({ newFileName, oldFileName }))
+                    } else {
+                      resolve({ newFileName })
+                    }
+                  }
+                }
+              )
+            })
         })
+        form.on('error', reject)
+        form.parse(request)
       })
-      .catch(() => {
-        fs.rename(
-          file.path,
-          path.join(__dirname, '../img', newFileName),
-          (error) => {
-            if (error) {
-              reject(error)
-            } else {
-              if (oldFileName) {
-                fs.unlink(path.join(__dirname, '../img', oldFileName), () => resolve({ newFileName, oldFileName }))
-              } else {
-                resolve({ newFileName })
-              }
-            }
-          }
-        )
-      })
-  })
-  form.on('error', reject)
-  form.parse(request)
-})
+    }
+
+    return false
+  }
+}
