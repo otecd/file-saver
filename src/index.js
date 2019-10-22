dcc84189efd0aef66351ac017ef627bc4ab6ec27
Dmitriy <idimzz@gmail.com>
2019-10-22 21:53:15 +0000

    fix upload dir path

--- src/index.js
+++ src/index.js
@@ -30,20 +30,21 @@ export default class ImageSaver {
     url,
     request,
     onlyReplacement,
-    blur
+    blur,
+    uploadDir
   } = {}) {
     if (url) {
       const newFileName = `${uuidv4(url)}.jpg`
 
       if (blur) {
-        const tempFilePath = path.join(__dirname, '../img', `temp_${newFileName}`)
+        const tempFilePath = path.join(uploadDir, `temp_${newFileName}`)
 
         return download(url, tempFilePath)
           .then(() => sharp(tempFilePath)
             .resize(100, 100)
             .blur(3)
             .jpeg({ quality: 60 })
-            .toFile(path.join(__dirname, '../img', newFileName)))
+            .toFile(path.join(uploadDir, newFileName)))
           .then(() => {
             fs.unlink(tempFilePath, () => {})
 
@@ -51,13 +52,13 @@ export default class ImageSaver {
           })
       }
 
-      return download(url, path.join(__dirname, '../img', newFileName))
+      return download(url, path.join(uploadDir, newFileName))
         .then(() => newFileName)
     }
 
     if (request) {
       return new Promise((resolve, reject) => {
-        const form = new IncomingForm({ uploadDir: path.join(__dirname, '../img'), keepExtensions: true })
+        const form = new IncomingForm({ uploadDir, keepExtensions: true })
 
         form.on('file', (_, file) => {
           if (file.name === 'temp.jpg' && onlyReplacement) {
@@ -66,34 +67,24 @@ export default class ImageSaver {
 
           const oldFileName = file.name !== 'temp.jpg' && file.name
           const newFileName = `${uuidv4()}.jpg`
+          const resultCb = (error) => {
+            if (error) {
+              reject(error)
+            } else {
+              if (oldFileName) {
+                fs.unlink(path.join(uploadDir, oldFileName), () => resolve({ newFileName, oldFileName }))
+              } else {
+                resolve({ newFileName })
+              }
+            }
+          }
 
           jo.rotate(file.path, { quality: 90 })
             .then(({ buffer }) => {
-              fs.writeFile(path.join(__dirname, '../img', newFileName), buffer, (error) => {
-                if (error) {
-                  return reject(error)
-                }
-                return oldFileName
-                  ? fs.unlink(path.join(__dirname, '../img', oldFileName), () => resolve({ newFileName, oldFileName }))
-                  : resolve({ newFileName })
-              })
+              fs.writeFile(path.join(uploadDir, newFileName), buffer, resultCb)
             })
             .catch(() => {
-              fs.rename(
-                file.path,
-                path.join(__dirname, '../img', newFileName),
-                (error) => {
-                  if (error) {
-                    reject(error)
-                  } else {
-                    if (oldFileName) {
-                      fs.unlink(path.join(__dirname, '../img', oldFileName), () => resolve({ newFileName, oldFileName }))
-                    } else {
-                      resolve({ newFileName })
-                    }
-                  }
-                }
-              )
+              fs.rename(file.path, path.join(uploadDir, newFileName), resultCb)
             })
         })
         form.on('error', reject)
