5ce2bd52ffbc8e90a1f343429be1f416c2ccd2c5
Dmitrii Pron <idimzz@gmail.com>
2020-02-07 05:47:36 +0000

    use errors lib and refactor the code

--- src/index.js
+++ src/index.js
@@ -1,93 +1,117 @@
 import path from 'path'
 import fs from 'fs'
 import uuidv4 from 'uuid/v4'
-import wget from 'node-wget'
+import wget from 'wget-improved'
 import jo from 'jpeg-autorotate'
 import { IncomingForm } from 'formidable'
 import sharp from 'sharp'
-import RichError from '@noname.team/rich-error'
+import { HttpError } from '@noname.team/errors'
 
-const download = (url, dest) => new Promise((resolve, reject) => {
+const download = ({
+  url,
+  to,
+  extensions = ['jpg', 'png'],
+  onStart = fileSize => fileSize,
+  onProgress = progress => progress,
+  wgetOptions = {}
+}) => new Promise((resolve, reject) => {
   const extension = url.split('.')
     .pop()
     .split('?')[0]
 
-  if (!['jpg', 'png'].includes(extension)) {
-    return reject(new RichError('Unsupported image format. Please, upload only JPG, PNG file.', 415, RichError.codes.UNSUPPORTED_IMAGE_TYPE))
+  if (!extensions.includes(extension)) {
+    return reject(new HttpError(415))
   }
-  return wget(
-    { url, dest, timeout: 10000 },
-    (error, response) => error ? reject(error) : resolve(response)
-  )
+
+  wget.download(url, to, wgetOptions)
+    .on('error', reject)
+    .on('start', onStart)
+    .on('progress', onProgress)
+    .on('end', resolve)
 })
 
+/**
+ * ImageSaver class. Use it for image downloading, processing and storing wherever you want.
+ */
 export default class ImageSaver {
-  constructor ({
-    url,
-    request,
-    onlyReplacement,
-    blur,
-    uploadDir
-  } = {}) {
-    if (url) {
-      const newFileName = `${uuidv4(url)}.jpg`
+  /**
+   * @param {Object} config - input configuration.
+   * @param {!string} config.targetDir - is output system directory.
+   * @param {?boolean} [config.onlyReplacement] - in case if you don't want to create a new file, e.g. limit case.
+   * @return {Object} - an instance.
+   */
+  constructor ({ targetDir, onlyReplacement } = {}) {
+    /**
+     * @property {string} targetDir
+     */
+    this.targetDir = targetDir
+    /**
+     * @property {boolean} onlyReplacement
+     */
+    this.onlyReplacement = onlyReplacement
+    /**
+     * @property {?Object} sharp
+     */
+    this.sharp = null
+    this.download = this.download.bind(this)
+  }
 
-      if (blur) {
-        const tempFilePath = path.join(uploadDir, `temp_${newFileName}`)
+  /**
+   * This method is a step 1 of image saving.
+   * @param {Object} config - input configuration.
+   * @param {!(string|Object)} config.source - image source. String means that this is URL or you can provide an Object that is for Http Request instance
+   * @param {?string} [config.targetFileName=`${uuidv4()}.jpg`] - is output file name.
+   * @param {?boolean} config.skipProcessing - if you want to skip sharp initializing and you just need the file
+   * @return {Promise<?Object, HttpError>} - return a Sharp object which you can handle in your way. Throw HttpError with a code 416 when this.onlyReplacement, but you are going to download a new file.
+   */
+  async download ({
+    source,
+    targetFileName = `${uuidv4()}.jpg`,
+    skipProcessing
+  }) {
+    const targetPath = path.join(this.targetDir, targetFileName)
 
-        return download(url, tempFilePath)
-          .then(() => sharp(tempFilePath)
-            .resize(100, 100)
-            .blur(3)
-            .jpeg({ quality: 60 })
-            .toFile(path.join(uploadDir, newFileName)))
-          .then(() => {
-            fs.unlink(tempFilePath, () => {})
+    switch (typeof source) {
+      case 'string': {
+        const sourceFileName = source.split('/')
+          .pop()
+          .split('?')[0]
 
-            return newFileName
-          })
+        if (this.onlyReplacement && sourceFileName !== targetFileName) {
+          throw new HttpError(416)
+        }
+
+        await download({ url: source, to: targetPath })
+        break
       }
+      default: {
+        await new Promise((resolve, reject) => {
+          const form = new IncomingForm({ uploadDir: this.targetDir, keepExtensions: true })
 
-      return download(url, path.join(uploadDir, newFileName))
-        .then(() => newFileName)
-    }
+          form.on('file', async (_, file) => {
+            let joResult
 
-    if (request) {
-      return new Promise((resolve, reject) => {
-        const form = new IncomingForm({ uploadDir, keepExtensions: true })
-
-        form.on('file', (_, file) => {
-          if (file.name === 'temp.jpg' && onlyReplacement) {
-            return reject(new RichError('Images limit is reached', 416, RichError.codes.REACHED_IMAGES_LIMIT))
-          }
-
-          const oldFileName = file.name !== 'temp.jpg' && file.name
-          const newFileName = `${uuidv4()}.jpg`
-          const resultCb = (error) => {
-            if (error) {
-              reject(error)
-            } else {
-              if (oldFileName) {
-                fs.unlink(path.join(uploadDir, oldFileName), () => resolve({ newFileName, oldFileName }))
-              } else {
-                resolve({ newFileName })
-              }
+            if (this.onlyReplacement && file.name !== targetFileName) {
+              return reject(new HttpError(416))
             }
-          }
 
-          jo.rotate(file.path, { quality: 90 })
-            .then(({ buffer }) => {
-              fs.writeFile(path.join(uploadDir, newFileName), buffer, resultCb)
-            })
-            .catch(() => {
-              fs.rename(file.path, path.join(uploadDir, newFileName), resultCb)
-            })
+            try {
+              joResult = await jo.rotate(file.path)
+              fs.writeFileSync(targetPath, joResult.buffer)
+            } catch (error) {
+              fs.renameSync(file.path, targetPath)
+            }
+            resolve()
+          })
+          form.on('error', reject)
+          form.parse(source)
         })
-        form.on('error', reject)
-        form.parse(request)
-      })
+        break
+      }
     }
 
-    return false
+    this.sharp = skipProcessing ? null : sharp(targetPath)
+
+    return this
   }
 }
