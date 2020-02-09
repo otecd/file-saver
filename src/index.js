7db7bd05db8491d1280d63f214cc2038b62cb00c
Dmitrii Pron <idimzz@gmail.com>
2020-02-09 13:05:17 +0000

    use only rich errors and add extensions option

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import uuidv4 from 'uuid/v4'
 import wget from 'wget-improved'
 import jo from 'jpeg-autorotate'
 import { IncomingForm } from 'formidable'
-import { HttpError, RichError } from '@noname.team/errors'
+import { RichError } from '@noname.team/errors'
+import { error_codes as errorCodes } from './const.json'
 
 const download = ({
   url,
@@ -14,16 +15,23 @@ const download = ({
   onProgress = progress => progress,
   wgetOptions = {}
 }) => new Promise((resolve, reject) => {
-  const extension = url.split('.')
+  let urlParsed
+
+  try {
+    urlParsed = new URL(url)
+  } catch (error) {
+    return reject(new RichError(error.message || 'Image source is broken', errorCodes.ERR_IMAGE_SOURCE_BROKEN))
+  }
+
+  const extension = urlParsed.pathname.split('.')
     .pop()
-    .split('?')[0]
 
   if (!extensions.includes(extension)) {
-    return reject(new HttpError(415))
+    return reject(new RichError('Unsupported image format', errorCodes.ERR_IMAGE_FORMAT_UNSUPPORTED))
   }
 
   wget.download(url, to, wgetOptions)
-    .on('error', reject)
+    .on('error', (error) => reject(new RichError(error.message || 'Image can not be loaded', errorCodes.ERR_IMAGE_CAN_NOT_BE_LOADED)))
     .on('start', onStart)
     .on('progress', onProgress)
     .on('end', resolve)
@@ -37,20 +45,29 @@ export default class ImageSaver {
    * @param {Object} config - input configuration.
    * @param {!string} config.targetDir - is output system directory.
    * @param {?boolean} [config.onlyReplacement] - in case if you don't want to create a new file, e.g. limit case.
+   * @param {?Array<string>} [config.validExtensions] - acceptable image extensions.
    * @return {Object} - an instance.
    */
-  constructor ({ targetDir, onlyReplacement } = {}) {
+  constructor ({
+    targetDir,
+    onlyReplacement,
+    validExtensions
+  } = {}) {
     /**
      * @type {Object}
      * @property {string} target.dir
      * @property {string} target.path
+     * @property {string} target.fileName
      */
     this.target = {
       dir: targetDir,
-      path: ''
+      path: null,
+      fileName: null
     }
     /** @property {boolean} */
     this.onlyReplacement = onlyReplacement
+    /** @property {boolean} */
+    this.validExtensions = validExtensions
 
     this.download = this.download.bind(this)
     this.process = this.process.bind(this)
@@ -58,13 +75,13 @@ export default class ImageSaver {
 
   /**
    * This method is a firstable step of image saving.
-   * @param {Object} config - input configuration.
-   * @param {!(string|Object)} config.source - image source. String means that this is URL or you can provide an Object that is for Http Request instance
-   * @param {?string} [config.targetFileName=`${uuidv4()}.jpg`] - is output file name.
-   * @return {Promise<Object, HttpError>} - return a current instance. Throw HttpError with a code 416 when this.onlyReplacement, but you are going to download a new file.
+   * @param {!(string|Object)} source - image source. String means that this is URL or you can provide an Object that is for Http Request instance
+   * @param {?string} [targetFileName=`${uuidv4()}.jpg`] - is output file name.
+   * @return {Promise<Object, Error>} - return a current instance. Throw an Error when this.onlyReplacement, and you are going to download a new file.
    */
-  async download ({ source, targetFileName = `${uuidv4()}.jpg` }) {
-    this.target.path = path.join(this.target.dir, targetFileName)
+  async download (source, targetFileName = `${uuidv4()}.jpg`) {
+    this.target.fileName = targetFileName
+    this.target.path = path.join(this.target.dir, this.target.fileName)
 
     switch (typeof source) {
       case 'string': {
@@ -72,11 +89,15 @@ export default class ImageSaver {
           .pop()
           .split('?')[0]
 
-        if (this.onlyReplacement && sourceFileName !== targetFileName) {
-          throw new RichError('Images limit is reached', 'ERR_IMAGES_LIMIT_REACHED')
+        if (this.onlyReplacement && sourceFileName !== this.target.fileName) {
+          throw new RichError('Images limit is reached', errorCodes.ERR_IMAGES_LIMIT_REACHED)
         }
 
-        await download({ url: source, to: this.target.path })
+        await download({
+          url: source,
+          to: this.target.path,
+          extensions: this.validExtensions
+        })
         break
       }
       default: {
@@ -86,8 +107,8 @@ export default class ImageSaver {
           form.on('file', async (_, file) => {
             let joResult
 
-            if (this.onlyReplacement && file.name !== targetFileName) {
-              return reject(new RichError('Images limit is reached', 'ERR_IMAGES_LIMIT_REACHED'))
+            if (this.onlyReplacement && file.name !== this.target.fileName) {
+              return reject(new RichError('Images limit is reached', errorCodes.ERR_IMAGES_LIMIT_REACHED))
             }
 
             try {
@@ -111,7 +132,7 @@ export default class ImageSaver {
   /**
    * This method is optional step for image processing.
    * @param {Object} transformer - sharp operations.
-   * @return {Promise<Object, HttpError>} - return a current instance. Throw errors when file system troubles.
+   * @return {Promise<Object, Error>} - return a current instance. Throw errors when file system troubles.
    */
   process (transformer) {
     return new Promise((resolve, reject) => {
